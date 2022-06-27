89c31fbcd16d1c5643d757ac3d693b0766a5571b
SungRim <wowilos@naver.com>
2022-06-27 16:40:36 +0900

    11

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,13 +1,9 @@
 <template>
-	<div><h2>** List **</h2></div>
+  <div><h2>** List **</h2></div>
 </template>
 
 <script>
-export default {
-	
-}
+export default {};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
