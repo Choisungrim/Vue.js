0b1a9abaf586757c4718a75b9046214bbde2c2f2
SungRim <wowilos@naver.com>
2022-06-28 10:38:57 +0900

    220628

--- vue-todolead/src/components/TodoHeader.vue
+++ vue-todolead/src/components/TodoHeader.vue
@@ -1,17 +1,15 @@
 <template>
-	<div><h1>Vue.js 일정관리</h1></div>
+  <div><h1>Vue.js 일정관리</h1></div>
 </template>
 
 <script>
-export default {
-	
-}
+export default {};
 </script>
 
 <style scoped>
-	h1 {
-		color: #2F3B52;
-    	font-weight: 900;
-    	margin: 2.5rem 0 1.5rem;
-	}
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
 </style>
--- vue-todolead/src/components/TodoList.vue
+++ vue-todolead/src/components/TodoList.vue
@@ -1,5 +1,11 @@
 <template>
-	<div><h2>** List **</h2></div>
+	<section>
+		<ul>
+			<li>일정 1</li>
+			<li>일정 2</li>
+			<li>일정 3</li>
+		</ul>
+	</section>
 </template>
 
 <script>
